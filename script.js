a4a21d774ff107479b6d80ffa40761d6c9a3899d
Merryl Arokiasamy <merrylarok@gmail.com>
2022-09-05 21:54:21 -0600

    Made infinite scroll logo banners, refactored css styles and improved aesthetics

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-var windowWidth = window.innerWidth;
-var windowHeight = window.innerHeight;
-var contentChunks = document.getElementsByClassName('chunk');
-for (var _i = 0, contentChunks_1 = contentChunks; _i < contentChunks_1.length; _i++) {
-    var chunk = contentChunks_1[_i];
-    chunk.style.width = windowWidth + "px";
-    chunk.style.height = windowHeight + "px";
-}
-window.addEventListener('resize', updateWindowSize);
+// // let windowWidth: Number = window.innerWidth;
+// // Using 100% width in CSS to prevent overflow due to vertical scrollbar 
+// let windowHeight: Number = window.innerHeight;
+// const contentChunks = document.getElementsByClassName('chunk') as HTMLCollectionOf<HTMLElement>;
+// for (let chunk of contentChunks) {
+//     // chunk.style.width = `${windowWidth}px`;
+//     chunk.style.height = `${windowHeight}px`;
+// }
+// window.addEventListener('resize', updateWindowSize);
 var yearsAlive = document.getElementById('yearsAlive');
 yearsAlive.innerText = (new Date().getFullYear() - 1998).toString() + " years";
-function updateWindowSize() {
-    windowWidth = window.innerWidth;
-    windowHeight = window.innerHeight;
-    for (var _i = 0, contentChunks_2 = contentChunks; _i < contentChunks_2.length; _i++) {
-        var chunk = contentChunks_2[_i];
-        chunk.style.width = windowWidth + "px";
-        chunk.style.height = windowHeight + "px";
-    }
-}
+// function updateWindowSize() {
+//     windowWidth = window.innerWidth;
+//     windowHeight = window.innerHeight;
+//     for (let chunk of contentChunks) {
+//         chunk.style.width = `${windowWidth}px`;
+//         chunk.style.height = `${windowHeight}px`;
+//     }
+// }
--- script.ts
+++ script.ts
@@ -1,26 +1,27 @@
-let windowWidth: Number = window.innerWidth;
-let windowHeight: Number = window.innerHeight;
+// // let windowWidth: Number = window.innerWidth;
+// // Using 100% width in CSS to prevent overflow due to vertical scrollbar 
+// let windowHeight: Number = window.innerHeight;
 
-const contentChunks = document.getElementsByClassName('chunk') as HTMLCollectionOf<HTMLElement>;
+// const contentChunks = document.getElementsByClassName('chunk') as HTMLCollectionOf<HTMLElement>;
 
-for (let chunk of contentChunks) {
-    chunk.style.width = `${windowWidth}px`;
-    chunk.style.height = `${windowHeight}px`;
-}
+// for (let chunk of contentChunks) {
+//     // chunk.style.width = `${windowWidth}px`;
+//     chunk.style.height = `${windowHeight}px`;
+// }
 
-window.addEventListener('resize', updateWindowSize);
+// window.addEventListener('resize', updateWindowSize);
+
+
+// function updateWindowSize() {
+//     windowWidth = window.innerWidth;
+//     windowHeight = window.innerHeight;
+
+//     for (let chunk of contentChunks) {
+//         chunk.style.width = `${windowWidth}px`;
+//         chunk.style.height = `${windowHeight}px`;
+//     }
+
+// }
 
 let yearsAlive = document.getElementById('yearsAlive') as HTMLElement;
-yearsAlive.innerText = (new Date().getFullYear() - 1998).toString()+" years";
-
-function updateWindowSize() {
-    windowWidth = window.innerWidth;
-    windowHeight = window.innerHeight;
-
-    for (let chunk of contentChunks) {
-        chunk.style.width = `${windowWidth}px`;
-        chunk.style.height = `${windowHeight}px`;
-    }
-
-}
-
+yearsAlive.innerText = (new Date().getFullYear() - 1998).toString() + " years";
